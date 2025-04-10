9c8d1a598c2e8cba5243167b39eac4a5e1bf7250
Jon <jonathan-kings@hotmail.com>
2025-04-10 17:36:00 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,33 @@
+
+const table = document.getElementsByClassName("table1")[0];
+if (table) {
+    const header = [...[...table.childNodes].find(node => node.tagName === 'THEAD').childNodes].find(node => node instanceof HTMLTableRowElement);
+    const a = document.createElement('a');
+    a.innerText = " 👉 Click here to sort by ratings 👈";
+    a.style.cursor = 'pointer';
+    header.cells[0].appendChild(a);
+    a.addEventListener('click', async () => {
+        a.remove();
+        const loadingIndicator = document.createTextNode(" (...)");
+        header.cells[0].appendChild(loadingIndicator);
+        done = true;
+        const tbody = [...table.childNodes].find(node => node.tagName === 'TBODY');
+        const domParser = new DOMParser;
+        await Promise.all([...tbody.rows].map(async row => {
+            const titleCell = row.cells[0];
+            const url = [...titleCell.childNodes].find(node => node.tagName === "A").href;
+            const doc = domParser.parseFromString(await (await fetch(url)).text(), "text/html");
+            const rating = parseFloat(doc.getElementsByClassName('current-rating')[0].childNodes[0].innerText);
+            titleCell.appendChild(document.createTextNode(` (${
+                new Array(Math.round(rating)).fill('⭐').join('')
+            })`));
+            row.rating = rating;
+        }));
+        const sorted = [...tbody.children].sort((a, b) => b.rating - a.rating);
+        for (const elem of sorted) {
+            tbody.appendChild(elem);
+        }
+        loadingIndicator.remove();
+        header.cells[0].appendChild(document.createTextNode(" (sorted by ratings 👍)"));
+    });
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+# walkhighlands-rated-sort
+
+Firefox browser extension to sort Walkhighlands walks by ratings.
+
+## Install
+
+Clone this repo locally (or just download the files).
+
+In Firefox, open [about:debugging](about:debugging), click the _This Firefox_ tab (top left), click the _Load Temporary Add-on_ button, then select the `manifest.json` file from this repo. The extension will remain active until Firefox is closed/restarted.
+
+Access [the WalkHighlands search page](https://www.walkhighlands.co.uk/walk-search.php) and enter your search parameters, then click _Search Now_. If the extension is properly installed, the table of results should have a "Click here to sort by ratings button" at the top, which will sort the table (from best rated to worst) upon being clicked.
